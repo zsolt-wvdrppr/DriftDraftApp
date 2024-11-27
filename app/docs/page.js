96e3c9ce823b93b6bcdffbf343c904a0fb614b5f
zsolt <admin@wavedropper.com>
2024-11-27 18:51:40 +0000

    Update page.js

--- app/docs/page.js
+++ app/docs/page.js
@@ -1,11 +1,11 @@
 import { title } from "@/components/primitives";
-import GoogleAiRequester from "@/components/googleAi/GoogleAiRequester";
+//import GoogleAiRequester from "@/components/googleAi/GoogleAiRequester";
 
 export default function DocsPage() {
   return (
     <div>
       <h1 className={title()}>Docs</h1>
-      <GoogleAiRequester prompt="tell a joke" />
+      {/*<GoogleAiRequester prompt="tell a joke" />*/}
     </div>
   );
 }
