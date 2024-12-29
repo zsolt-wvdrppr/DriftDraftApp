2b6fac4a0bf9a65f7ee30babcb23d446354022b0
zsolt <admin@wavedropper.com>
2024-12-29 15:07:32 +0000

    Update PasteButton.js

--- components/websitePlanner/layout/PasteButton.js
+++ components/websitePlanner/layout/PasteButton.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import logger from "@/lib/logger";
-import { Button } from "@nextui-org/react";
+import { Link } from "@nextui-org/react";
 import { IconClipboard } from "@tabler/icons-react";
 import { Tooltip } from 'react-tooltip';
 
 const PasteButton = ({ value, handleChange, setError, children }) => {
-
+    
     const handlePaste = async () => {
         try {
             // Read text from clipboard
@@ -16,12 +16,8 @@ const PasteButton = ({ value, handleChange, setError, children }) => {
 
             // Call the existing change handler to ensure everything syncs properly
             handleChange({ target: { value: updatedValue } });
-
-            // Show the tooltip temporarily and hide it after 5 seconds
-            Tooltip.hide(document.querySelector('.paste-btn'), { delayHide: 5000 });
-
-        } catch (error) {
             
+        } catch (error) {
             logger.info("Failed to read from clipboard", error);
             setError("You need to grant clipboard access in your browser to use this feature.");
         }
@@ -29,12 +25,12 @@ const PasteButton = ({ value, handleChange, setError, children }) => {
 
     return (
         <div className="relative">
-            <Button id="paste-btn" className="paste-btn absolute z-10 right-0" onPress={handlePaste}>
+            <Link variant="none" id="paste-btn" className="paste-btn absolute z-10 right-0 text-neutralSnow pr-1 pt-2" onPress={handlePaste}>
                 <IconClipboard />
-            </Button>
-            <Tooltip anchorSelect=".paste-btn" place="top" className="text-center" delayHide={200} delayShow={100}>
-                Paste from clipboard
-            </Tooltip>
+            </Link>
+            <Tooltip anchorSelect=".paste-btn" place="top" className="text-center" delayHide={500} delayShow={200}>
+                    Paste from clipboard
+                </Tooltip>
             {children}
         </div>
     );
