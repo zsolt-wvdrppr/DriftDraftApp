d0727edf0809004fba8a19f6cff3f587d068b9b5
zsolt <admin@wavedropper.com>
2025-03-11 13:30:51 +0000

    minor update

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -2,8 +2,16 @@ import { title } from "@/components/primitives";
 
 export default function PricingPage() {
   return (
-    <div>
-      <h1 className={title()}>Pricing</h1>
+    <div className="flex flex-col items-center justify-center h-full gap-y-10">
+      <h1 className={`${title()}`}>Pricing</h1>
+      <p className="italic text-primary">{`Detailed pricing's coming soon`}</p>
+      <div className="text-lg">
+        <span>DriftDraft.App operates on a credit-based system:</span><br /><br />
+        <ul className="list-disc list-inside text-left">
+        <li><strong>AI Hint Generation:</strong> 1 credit per AI hint.</li>
+        <li><strong>Plan Generation:</strong> 4 credits per full plan.</li>
+        </ul>
+      </div>
     </div>
   );
 }
