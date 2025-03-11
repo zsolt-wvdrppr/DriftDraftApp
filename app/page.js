1e038da04732ae6ba33b421d1cfc900b1ded78f3
zsolt <admin@wavedropper.com>
2025-03-11 11:21:47 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <ServiceTitle />
 
-      <div class="mx-2 bg-yellow-100 border-l-4 border-yellow-400 text-yellow-800 p-4 rounded-xl text-sm max-w-xl self-center mb-6">
+      <div class="mx-2 bg-yellow-100 border-l-4 border-yellow-400 text-yellow-800 p-4 rounded-xl text-sm max-w-xl self-center mt-6">
         <p class="font-medium">Beta Notice</p>
         <p>
           {`This app is currently in beta, so you might encounter minor bugs or
@@ -20,7 +20,7 @@ export default function Home() {
       <ServiceSelector />
 
       <div className="mt-8">
-        <pre className="text-justify md:text-center border-2 border-gray-300 p-4 rounded-lg">
+        <pre className="text-justify md:text-center border-2 border-gray-300 p-4 mx-2 rounded-lg">
           <span className="whitespace-break-spaces">
             {
               "DriftDraft is a strategic website and landing page planner that helps users outline their site's structure, user journey, and development requirements through guided questions and AI-driven recommendations."
