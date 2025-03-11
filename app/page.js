488e8493335fd734dd38bf8735566052ff250e7f
zsolt <admin@wavedropper.com>
2025-03-11 11:18:43 +0000

    Add Beta Notice to homepage

--- app/page.js
+++ app/page.js
@@ -4,15 +4,27 @@ import ServiceTitle from "@/components/ServiceTitle";
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      
       <ServiceTitle />
+
+      <div class="mx-2 bg-yellow-100 border-l-4 border-yellow-400 text-yellow-800 p-4 rounded-xl text-sm max-w-xl self-center mb-6">
+        <p class="font-medium">Beta Notice</p>
+        <p>
+          {`This app is currently in beta, so you might encounter minor bugs or
+          unfinished features. We’re working hard to improve it!`}
+        </p>
+        <p class="mt-2">
+          {`Spotted something odd? Feel free to use the feedback form — it’ll automatically attach a screenshot of the page you’re on.`}
+        </p>
+      </div>
 
       <ServiceSelector />
 
       <div className="mt-8">
         <pre className="text-justify md:text-center border-2 border-gray-300 p-4 rounded-lg">
           <span className="whitespace-break-spaces">
-            {"DriftDraft is a strategic website and landing page planner that helps users outline their site's structure, user journey, and development requirements through guided questions and AI-driven recommendations."}
+            {
+              "DriftDraft is a strategic website and landing page planner that helps users outline their site's structure, user journey, and development requirements through guided questions and AI-driven recommendations."
+            }
           </span>
         </pre>
       </div>
