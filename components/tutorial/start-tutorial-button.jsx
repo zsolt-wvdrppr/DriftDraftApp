53bdad418b42c83fd058fee23f3983f6dac55a6d
zsolt <admin@wavedropper.com>
2025-05-01 21:14:00 +0100

    Update start-tutorial-button.jsx

--- components/tutorial/start-tutorial-button.jsx
+++ components/tutorial/start-tutorial-button.jsx
@@ -36,7 +36,7 @@ export const StartTutorialButton = ({ setStartTutorial, className, ...props }) =
   }
 
   return (
-    <div {...props} className="fixed top-100 right-0 z-50">
+    <div {...props} className="fixed top-100 right-2 z-50 rounded-full backdrop-blur-sm flex items-center justify-center bg-secondary/10">
       {isVisible && (
         <motion.div
           initial={{ opacity: 0, x: 20 }}
@@ -44,12 +44,13 @@ export const StartTutorialButton = ({ setStartTutorial, className, ...props }) =
           transition={{ duration: 0.7 }}
         >
           <Button
-            className={className || "transition-all duration-200 text-highlightOrange pt-4 pr-2 rounded-r-none rounded-t-none rounded-bl-full min-w-0"}
+            className={className || "transition-all flex items-center justify-center duration-200 px-0 py-0 text-highlightOrange min-h-0 h-fit w-fit min-w-0"}
             onPress={handleOnPress}
             title="Start tutorial"
           >
             <motion.div
               animate={{ y: [0, -4, 0] }}
+              className="flex items-center justify-center"
               transition={{ 
                 duration: 1.5, 
                 repeat: Infinity, 
@@ -57,7 +58,7 @@ export const StartTutorialButton = ({ setStartTutorial, className, ...props }) =
                 delay: 0.3
               }}
             >
-              <IconLifebuoyFilled size={24} />
+              <IconLifebuoyFilled size={24} className="-mb-1"/>
             </motion.div>
           </Button>
         </motion.div>
