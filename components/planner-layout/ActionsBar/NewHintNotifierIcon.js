6e89d385fe7aaa03dd2cf8a428134a8490e7081c
zsolt <admin@wavedropper.com>
2025-05-01 17:06:38 +0100

    improve hint button

--- components/planner-layout/ActionsBar/NewHintNotifierIcon.js
+++ components/planner-layout/ActionsBar/NewHintNotifierIcon.js
@@ -4,12 +4,17 @@ const NewHintNotifierIcon = ({ trigger }) => {
   return (
     <>
       {trigger ?
+      <div className="relative">
+        <div className={`absolute ${trigger ? "animate-bounce" : ""}`}>
+        <IconBulb className={`text-brandPink ${trigger ? "animate-ping" : ""}`} size={32}/>
+        </div>
         <IconBulbFilled
-          className={`top-2 left-1 ${trigger ? "text-brandPink dark:text-yellow-400 animate-bounce" : " text-brandPink"}`}
+          className={`top-2 left-1 ${trigger ? "text-brandPink animate-bounce" : " text-brandPink"}`}
           size={32}
         />
+        </div>
       : <IconBulb
-          className={`top-2 left-1 ${trigger ? "text-brandPink dark:text-yellow-400 animate-bounce" : "text-brandPink"}`}
+          className={`top-2 left-1 ${trigger ? "text-brandPink" : "text-brandPink"}`}
           size={32}
         />
       }
