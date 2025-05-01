ec5d92c3dbc0e2c1a48e233340c9e76786b442b5
zsolt <admin@wavedropper.com>
2025-05-01 09:53:21 +0100

    Update ServiceTitle.js

--- components/ServiceTitle.js
+++ components/ServiceTitle.js
@@ -6,27 +6,25 @@ import { useState, useEffect } from "react";
 import { title } from "@/components/primitives";
 
 const ServiceTitle = () => {
-    const words = ["Website", "Website Blueprint", "Landing Page"];
+    const words = ["Website", "Landing Page"];
     const [currentWordIndex, setCurrentWordIndex] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
-        }, 3000); // Change every 3 seconds
+        }, 5000); // Change every 3 seconds
 
         return () => clearInterval(interval);
     }, []);
 
     return (
-        <div className="max-w-xl text-center justify-center min-w-max">
-            <span className={`${title({ color: "violet" })}`}>Create a&nbsp;</span>
-            <span className={title()}>Strategic&nbsp;</span>
-            <br />
+        <div className="max-w-xl flex flex-col text-center justify-center min-w-max">
+            <span className={`${title({ color: "violet" })}`}>Plan your perfect&nbsp;</span>
             <AnimatePresence mode="wait">
                 <motion.span
                     key={words[currentWordIndex]}
                     animate={{ opacity: 1, y: 0 }}
-                    className={`${title({ color: "blue" })}`}
+                    className={`${title({ color: "blue" })} lg:h-14 lg:mt-4`}
                     exit={{ opacity: 0, y: 10 }}
                     initial={{ opacity: 0, y: -10 }}
                     transition={{ duration: 0.5 }}
