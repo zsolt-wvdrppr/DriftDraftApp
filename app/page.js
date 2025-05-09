36b36c658cddb8f1596fb56e5b1f64d080dbeb3f
zsolt <admin@wavedropper.com>
2025-05-09 13:23:08 +0100

    add og

--- app/page.js
+++ app/page.js
@@ -30,3 +30,30 @@ export default function Home() {
     </>
   );
 }
+
+export async function generateMetadata() {
+  return {
+    title: `Website Blueprint with DriftDraft.App`,
+    description: `DriftDraft is a strategic planner that transforms your business requirements into a comprehensive website or landing page blueprint.`,
+    icons: {
+      icon: '/favicon.ico',
+    },
+    openGraph: {
+      type: 'website',
+      locale: 'en_GB',
+      url: 'https://driftdraft.app',
+      siteName: 'DriftDraft.App',
+      title: 'Website Blueprint with DriftDraft.App',
+      description:
+        'DriftDraft is a strategic planner that transforms your business requirements into a comprehensive website or landing page blueprint.',
+      images: [
+        {
+          url: 'https://driftdraft.app/og-image.webp',
+          width: 1200,
+          height: 630,
+          alt: 'The Website Blueprint Creator App',
+        },
+      ],
+    },
+  };
+}
