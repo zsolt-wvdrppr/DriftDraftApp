014c1bdcf3b4a662030b10079e93b7f6aa2cae21
zsolt <admin@wavedropper.com>
2025-07-24 18:12:28 +0100

    add base

---
+++ wave-cms-manifest.js
@@ -0,0 +1,10 @@
+// wave-cms-manifest.js
+export default {
+  contentCollections: [
+    {
+      path: "content/blog-posts.js",
+      contentExport: "blogPosts",
+      schemaExport: "blogPostsSchema"
+    },
+  ],
+}
