c28eb9476b13c526b566066e3930b3bed8556fd2
ZS <164944009+admin-wavedropper@users.noreply.github.com>
2025-07-25 21:07:22 +0200

    Bulk update: 3 items across 1 collections (3 new items)
    Updated files:
    - blog-posts: blog-post-h9jx8mz0gg-556j4r6l, blog-post-h9jx7py5x8-263fh115, blog-post-h9jx59pwxs-4e2j4p3u

--- content/blog-posts.js
+++ content/blog-posts.js
@@ -1,4 +1,26 @@
-export const blogPosts = [];
+export const blogPosts = [
+{
+    id: "blog-post-h9jx59pwxs-4e2j4p3u",
+    title: "The Ultimate Guide to Strategic Website Planning",
+    content: "asdf",
+    featuredImage: "",
+    publishSchedule: null
+  },
+{
+    id: "blog-post-h9jx7py5x8-263fh115",
+    title: "How AI is Transforming Web Development in 2025",
+    content: "fdsa",
+    featuredImage: "",
+    publishSchedule: null
+  },
+{
+    id: "blog-post-h9jx8mz0gg-556j4r6l",
+    title: "Maximising Performance with Next.js 15 and React 19",
+    content: "turd",
+    featuredImage: "",
+    publishSchedule: null
+  }
+];
 
 export const blogPostsSchema = {
   name: "Blog Post",
