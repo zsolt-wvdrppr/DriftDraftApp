20f332f0b10c4f93fcaa63829b989f9973c2a8a8
zsolt <admin@wavedropper.com>
2025-08-17 20:29:57 +0100

    Improve text area ux
    strip markdown if formatted text pasted using native paste, while keeping native browser undo redo functionality in tact

--- components/planner-layout/layout/sectionComponents.js
+++ components/planner-layout/layout/sectionComponents.js
@@ -1,59 +1,87 @@
-import React from 'react';
-import { Textarea } from '@heroui/react';
+import React from "react";
+import { Textarea } from "@heroui/react";
 
-// eslint-disable-next-line import/order
-import { cn } from '@/lib/utils/utils';
+import { cn } from "@/lib/utils/utils";
+import { handlePasteEvent } from "@/lib/utils/clipboardUtils";
 
-export const StepOuterWrapper = ({ className = '', children }) => {
-
-const _className = " " + className;
+export const StepOuterWrapper = ({ className = "", children }) => {
+  const _className = " " + className;
 
   return (
-    <div className={cn("flex flex-col md:grid md:grid-cols-4 gap-2 md:gap-6 md:py-10 max-w-screen-xl px-2 md:px-0" + _className)}>
+    <div
+      className={cn(
+        "flex flex-col md:grid md:grid-cols-4 gap-2 md:gap-6 md:py-10 max-w-screen-xl px-2 md:px-0" +
+          _className
+      )}
+    >
       {children}
     </div>
-  )
-}
+  );
+};
 
-export const StepInnerWrapper = ({ className = '', children }) => {
-
+export const StepInnerWrapper = ({ className = "", children }) => {
   const _className = " " + className;
 
   return (
     <div className={cn("col-span-3 flex-1 md:space-y-4" + _className)}>
       {children}
     </div>
-  )
-}
+  );
+};
 
-import Sidebar from '@/components/planner-layout/ActionsBar/Sidebar'
+import Sidebar from "@/components/planner-layout/ActionsBar/Sidebar";
 
-export const StepWrapper = ({ whyDoWeAsk, userMsg, hint, className = null, children, checkDomain }) => {
-  
+export const StepWrapper = ({
+  whyDoWeAsk,
+  userMsg,
+  hint,
+  className = null,
+  children,
+  checkDomain,
+}) => {
   return (
     <StepOuterWrapper className={className}>
-      <StepInnerWrapper>
-        {children}
-      </StepInnerWrapper>
-      <Sidebar checkDomain={checkDomain} hint={hint} userMsg={userMsg} whyDoWeAsk={whyDoWeAsk}/>
+      <StepInnerWrapper>{children}</StepInnerWrapper>
+      <Sidebar
+        checkDomain={checkDomain}
+        hint={hint}
+        userMsg={userMsg}
+        whyDoWeAsk={whyDoWeAsk}
+      />
     </StepOuterWrapper>
-  )
-}
+  );
+};
 
-export const StepQuestion = ({ content, question, className = '' }) => {
-
+export const StepQuestion = ({ content, question, className = "" }) => {
   // check if content is an array
 
   const _className = " " + className;
 
   return (
-    <h2 className={cn("text-sm md:text-medium lg:text-lg font-semibold mb-4 text-primary dark:text-zinc-200 whitespace-break-spaces" + _className)}>
-      {question ? question : content?.question} {content?.required && <span className="text-red-500">*</span>}
+    <h2
+      className={cn(
+        "text-sm md:text-medium lg:text-lg font-semibold mb-4 text-primary dark:text-zinc-200 whitespace-break-spaces" +
+          _className
+      )}
+    >
+      {question ? question : content?.question}{" "}
+      {content?.required && <span className="text-red-500">*</span>}
     </h2>
-  )
-}
+  );
+};
 
-export const StepTextarea = ({ content, label, localValue, handleTextareaChange, isRequired, isInputInvalid, placeholder, className = '' }) => {
+export const StepTextarea = ({
+  content,
+  label,
+  localValue,
+  handleTextareaChange,
+  isRequired,
+  isInputInvalid,
+  placeholder,
+  className = "",
+  setError = () => {},
+}) => {
+
   return (
     <Textarea
       className={cn("p-1", className)}
@@ -70,14 +98,11 @@ export const StepTextarea = ({ content, label, localValue, handleTextareaChange,
       maxRows={25}
       minRows={4}
       placeholder={placeholder || content.placeholder}
-      validationBehavior='aria'
+      validationBehavior="aria"
       value={localValue}
       onChange={handleTextareaChange}
-      onClear={() => handleTextareaChange({ target: { value: '' } })}
+      onClear={() => handleTextareaChange({ target: { value: "" } })}
+      onPaste={(e) => handlePasteEvent(e, localValue, handleTextareaChange)}
     />
-  )
-}
-
-
-
-
+  );
+};
